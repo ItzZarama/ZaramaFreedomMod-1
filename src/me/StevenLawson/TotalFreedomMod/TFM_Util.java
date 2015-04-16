2b2474e6940e275422e44062eef5a77fbeba367b
Nathan Curran <darkeagle11221@gmail.com>
2015-04-16 19:26:47 +1000

    Don't forget about me!

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -68,7 +68,7 @@ public class TFM_Util
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
-    public static final List<String> ZFM_DEVELOPERS = Arrays.asList("Alex33856", "xxlinkdevil06xx", "iSaplings");
+    public static final List<String> ZFM_DEVELOPERS = Arrays.asList("Alex33856", "xxlinkdevil06xx", "iSaplings", "DaBoyTM");
     public static final List<String> FOUNDERS = Arrays.asList("ItzZarama", "Marius_nightfire");
     public static final List<String> SYSADMIN = Arrays.asList("NOOE");
     public static final List<String> HELPERS = Arrays.asList("NullOne")
