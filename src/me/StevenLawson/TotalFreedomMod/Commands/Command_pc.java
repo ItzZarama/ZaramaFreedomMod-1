cf294fdf3da0a383a3f61b9bca711ab4d456f9d8
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-19 12:41:46 -0700

    Created a command to clear your chat!

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_pc.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_pc.java
@@ -33,3 +33,4 @@ public class Command_pc extends TFM_Command
         sender.sendMessage(" ");
         sender.sendMessage(" ");
         sender.sendMessage(" ");
+        sender.sendMessage(ChatColor.RED "Your chat has been cleared!!!!")
