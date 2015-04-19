d952840e67bf30ab8e9ffce0776721d4d09f2e59
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-19 12:43:44 -0700

    Ooooh, it has to be  fixed! well it just got fixed

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_pex.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_pex.java
@@ -12,9 +12,9 @@ public class Command_pex extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-         sender.sendMessage(ChatColor.GREEN + "Why do you want admin so bad???");
-         sender.sendMessage(ChatColor.BLUE + "You might as well apply!!!!");
-         sender.sendMessage(ChatColor.GREEN + "I Can't belive you even tryed to super your self");
-         sender.sendMessage(ChatColor.BLUE + "Try using /Manuadd, Thats what you use to super your self here");
-         # Manuadd will troll them even more :), untill they find /saconfig :) 
+       sender.sendMessage(ChatColor.GREEN + "Why do you want admin so bad???");
+       sender.sendMessage(ChatColor.BLUE + "You might as well apply!!!!");
+       sender.sendMessage(ChatColor.GREEN + "I Can't belive you even tryed to super your self");
+       sender.sendMessage(ChatColor.BLUE + "Try using /Manuadd, Thats what you use to super your self here");
+       # Manuadd will troll them even more :), untill they find /saconfig :) 
     
