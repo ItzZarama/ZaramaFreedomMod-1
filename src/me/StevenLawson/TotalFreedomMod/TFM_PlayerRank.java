e87fc8dd57885011f02cf311682f61b31e4f227c
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-20 12:24:29 -0700

    There it is now fixed here.

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -7,7 +7,7 @@ import static me.StevenLawson.TotalFreedom.TFM_Util.FOUNDERS;
 import static me.StevenLawson.TotalFreedom.TFM_Util.SYSADMIN;
 import static me.StevenLawson.TotalFreedom.TFM_Util.HELPERS;
 import static me.StevenLawson.TotalFreedomMod.TFM_Util.TRYADMINS;
-import static me.StevenLawson.TotalFreedomMod.TFM_Util.ZFM_LeadDevelopers
+import static me.StevenLawson.TotalFreedomMod.TFM_Util.ZFM_LEADDEVELOPERS
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -28,7 +28,7 @@ public enum TFM_PlayerRank
     SYSADMIN("A " + ChatColor.GREEN + "System Admin", ChatColor.GREEN + "[SysAdmin]"),
     HELPERS("A " + ChatColor.RED + "Helper", ChatColor.RED + "[Helper]"),
     TRYADMINS("A" + ChatColor.GOLD + "Trial Super Admin", ChatColor.YELLOW + "[TRIALADMIN]"),
-    ZFM_LeadDevelopers("The" + ChatColor.DARK_PURPLE + "Lead Develope!", ChatColor.DARK_PURPLE + "[Lead Developer]");
+    ZFM_LEADDEVELOPERS("The" + ChatColor.DARK_PURPLE + "Lead Develope!", ChatColor.DARK_PURPLE + "[Lead Developer]");
     private final String loginMessage;
     private final String prefix;
 
@@ -107,9 +107,9 @@ public enum TFM_PlayerRank
             return TRYADMINS;
         }
         
-        if (ZFM_LeadDeveloper.contains(sender.getName()))
+        if (ZFM_LEADDEVELOPERS.contains(sender.getName()))
         {
-            return ZFM_LeadDeveloper;
+            return ZFM_LEADDEVELOPERS;
         {
         final TFM_Admin entry = TFM_AdminList.getEntryByIp(TFM_Util.getIp((Player) sender));
 
