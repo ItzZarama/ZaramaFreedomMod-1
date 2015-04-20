9993631935f5b2500da0c4faceff98ff864b3686
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-20 12:20:38 -0700

    Step 2 Done!
    -- Release 2.5.2 --
    Well, i am almost done with this rank all thats left is the Auto-Tag

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -7,6 +7,7 @@ import static me.StevenLawson.TotalFreedom.TFM_Util.FOUNDERS;
 import static me.StevenLawson.TotalFreedom.TFM_Util.SYSADMIN;
 import static me.StevenLawson.TotalFreedom.TFM_Util.HELPERS;
 import static me.StevenLawson.TotalFreedomMod.TFM_Util.TRYADMINS;
+import static me.StevenLawson.TotalFreedomMod.TFM_Util.ZFM_LeadDevelopers
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,17 +17,18 @@ public enum TFM_PlayerRank
     DEVELOPER("A " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[Dev]"),
     IMPOSTOR("An " + ChatColor.YELLOW + ChatColor.UNDERLINE + "Impostor", ChatColor.YELLOW.toString() + ChatColor.UNDERLINE + "[IMP]"),
     NON_OP("a " + ChatColor.GREEN + "Non-OP", ChatColor.GREEN.toString()),
-    OP("An " + ChatColor.RED + "OP", ChatColor.RED + "[OP]"),
+    OP("An " + ChatColor.RED + "OP", ChatColor.RED + "[Operator]"),
     SUPER("A " + ChatColor.GOLD + "Super Admin", ChatColor.GOLD + "[SA]"),
     TELNET("A " + ChatColor.DARK_GREEN + "Super Telnet Admin", ChatColor.DARK_GREEN + "[STA]"),
     SENIOR("A " + ChatColor.LIGHT_PURPLE + "Senior Admin", ChatColor.LIGHT_PURPLE + "[SrA]"),
     OWNER("The " + ChatColor.BLUE + "Owner", ChatColor.BLUE + "[Owner]"),
     CONSOLE("The " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]"),
-    ZFM_DEVELOPERS("a " + ChatColor.DARK_PURPLE + "Creator of ZFM", ChatColor.DARK_PURPLE + "[Developer]"),
+    ZFM_DEVELOPERS("a " + ChatColor.DARK_PURPLE + "Developer!", ChatColor.DARK_PURPLE + "[Developer]"),
     FOUNDERS("The " + ChatColor.DARK_RED + "Founder Of Zarama Freedom", ChatColor.DARK_RED + "[Founder]"),
     SYSADMIN("A " + ChatColor.GREEN + "System Admin", ChatColor.GREEN + "[SysAdmin]"),
     HELPERS("A " + ChatColor.RED + "Helper", ChatColor.RED + "[Helper]"),
-    TRYADMINS("A" + ChatColor.GOLD + "Trial Super Admin", ChatColor.YELLOW + "[TRIALADMIN]");
+    TRYADMINS("A" + ChatColor.GOLD + "Trial Super Admin", ChatColor.YELLOW + "[TRIALADMIN]"),
+    ZFM_LeadDevelopers("The" + ChatColor.DARK_PURPLE + "Lead Develope!", ChatColor.DARK_PURPLE + "[Lead Developer]");
     private final String loginMessage;
     private final String prefix;
 
@@ -104,6 +106,11 @@ public enum TFM_PlayerRank
         {
             return TRYADMINS;
         }
+        
+        if (ZFM_LeadDeveloper.contains(sender.getName()))
+        {
+            return ZFM_LeadDeveloper;
+        {
         final TFM_Admin entry = TFM_AdminList.getEntryByIp(TFM_Util.getIp((Player) sender));
 
         final TFM_PlayerRank rank;
