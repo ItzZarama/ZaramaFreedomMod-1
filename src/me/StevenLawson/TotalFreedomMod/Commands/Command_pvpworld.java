6f0c89dde5bfb833cb59e4bf06e0b0941f36af80
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-22 13:33:14 -0700

    @DaBoyTM, class name...
    My sister made me type this. :(

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_pvpworld.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_pvpworld.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Goto the pvp world.", usage = "/<command>")
-public class Command_flatlands extends TFM_Command
+public class Command_pvplands extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
