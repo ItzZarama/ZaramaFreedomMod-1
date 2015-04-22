57d4919eb85adcffb088808348f2d197b35386d3
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-22 13:29:56 -0700

    Does this seem good?

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmode.java
@@ -33,7 +33,7 @@ public class Command_adminmode extends TFM_Command
             {
                 if (!TFM_AdminList.isSuperAdmin(player))
                 {
-                    player.kickPlayer(sender.getname + ", has closed the server to all ops!);
+                    player.kickPlayer(sender.getname + ", has closed the server to non-superadmins. Please come back soon.);
                 }
             }
             return true;
