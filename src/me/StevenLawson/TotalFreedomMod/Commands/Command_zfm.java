e492755b6de55757f1133834a9d8d44d9d0624bd
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-04-23 12:07:01 -0700

    Lets update it this time, Oh wait i just did

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_zfm.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_zfm.java
@@ -14,8 +14,8 @@ public class Command_zfm extends TFM_Command
     {
         sender.sendMessage(ChatColor.GREEN + "This ZaramaFreedomMod is currently a fork from TotalFreedomMod!");
         sender.sendMessage(ChatColor.GOLD + "To Make your own FreedomMod, visit https://github.com/TotalFreedom/TotalFreedomMod for more info!");
-        sender.sendMessage(ChatColor.PURPLE + "Our Main Developers of this ZFM are Alex333856 and DaBoyTM!");
-        sender.sendMessage(ChatColor.RED + "ZFM Version: v2.5.3 BETA");
+        sender.sendMessage(ChatColor.PURPLE + "This version was coded by DaBoyTM and AlexFreedomMod");
+        sender.sendMessage(ChatColor.RED + "ZFM Version: v2.6");
         sender.sendMessage(ChatColor.BLUE + "For more info, please visit our forum at http://zaramafreedom.proboards.com/");
         sender.sendMessage(ChatColor.BLUE + "Or contact to one of our Developers :)");
         sender.sendMessage(ChatColor.BLUE + "If you would like something added, Please make an Pull request or if you find a issue go to" + ChatColor.GOLD + " http://github.com/AlexFreedomMod/ZaramaFreedomMod/issues");
